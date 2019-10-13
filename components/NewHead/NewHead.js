2e9facebea9cc4bcd2f95d424534aea61a37b59c
kurt miller <kmiller9393@gmail.com>
2019-10-13 16:43:23 -0600

    Add NewHead component to be reused throughout pages

---
+++ components/NewHead/NewHead.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+const NewHead = () => {
+  return (
+    <Head>
+      <link rel="stylesheet" type="text/css" href="/static/styles.css" />
+      <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
+      <title>Peart St Surfboards</title>
+    </Head>
+  );
+};
+
+export default NewHead;
